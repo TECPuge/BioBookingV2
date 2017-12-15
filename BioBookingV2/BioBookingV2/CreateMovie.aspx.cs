da919cc7006dc1eb7c25c887851cadd4711116dc
Martin Puge Nielsen <MartinNielsen@Deltek.com>
2017-12-16 00:11:18 +0100

    Basic of Opret film

---
+++ BioBookingV2/BioBookingV2/CreateMovie.aspx.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using BioBookingV2.DTO;
+using BioBookingV2.DAL;
+namespace BioBookingV2
+{
+    public partial class CreateMovie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            decimal Price = 0;
+            // Todo error handling on decimal (Raise friendly error dialog)
+            if (Decimal.TryParse(InputPrice.Value, out Price))
+            {
+                MovieDTO NewMovie = new MovieDTO
+                {
+                    Title = InputTitle.Value,
+                    Description = InputDescription.Value,
+                    PosterFileName = "/NotMadeYet.png",
+                    Price = Price
+                };
+
+                SQLConnector con = new SQLConnector();
+
+                NewMovie = con.CreateMovie(NewMovie);
+            }
+
+        }
+    }
+}
