af93f1ca8a7adcfa35a5002478b3f998ba56e2a1
Martin Puge Nielsen <MartinNielsen@Deltek.com>
2017-12-15 18:54:39 +0100

    Tilføjede DTO'er på ny.......

---
+++ BioBookingV2/BioBookingV2/DTO/SeatDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BioBookingV2.DTO
+{
+    public class SeatDTO
+    {
+        public int Id { get; set; }
+        public int TheaterId { get; set; }
+        public int SeatRow { get; set; }
+        public int SeatNumber { get; set; }
+        public bool Blocked { get; set; }
+    }
+}
