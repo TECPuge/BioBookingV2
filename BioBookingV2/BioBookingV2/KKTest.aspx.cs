0551942b7c94393ebb23f20cea11a7ec20845469
triksain <kristian_kamp@hotmail.com>
2017-12-15 22:17:21 +0100

    CreateMovie function til SQLConnector

--- BioBookingV2/BioBookingV2/KKTest.aspx.cs
+++ BioBookingV2/BioBookingV2/KKTest.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BioBookingV2.DAL;
+using BioBookingV2.DTO;
 using System.Reflection;
 namespace BioBookingV2
 {
@@ -12,9 +13,25 @@ namespace BioBookingV2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SQLConnector con = new SQLConnector();
             ControlLabel.Text = "Control";
-            TestLabel.Text = con.test();
+            //SQLConnector con = new SQLConnector();
+            //MovieDTO movie1 = new MovieDTO();
+            //con.CreateMovie(movie1);
+            //MovieDTO movie3 = new MovieDTO()
+            //{
+            //    Description = "Description Only"
+            //};
+            //con.CreateMovie(movie3);
+            //MovieDTO movie4 = new MovieDTO()
+            //{
+            //    PosterFileName = "PosterFileName Only"
+            //};
+            //con.CreateMovie(movie4);
+            //MovieDTO movie5 = new MovieDTO()
+            //{
+            //    Price = 13.37M
+            //};
+            //con.CreateMovie(movie5);
         }
 
         protected void SubmitButton_Click(object sender, EventArgs e)
@@ -27,6 +44,19 @@ namespace BioBookingV2
                 strTemp += pi.Name + ": " + pi.GetValue(obj) + "<br />";
             }
             TestLabel.Text = strTemp;
+        }
+
+        protected void SeedButton_Click(object sender, EventArgs e)
+        {
+            SQLConnector con = new SQLConnector();
+            MovieDTO movie2 = new MovieDTO()
+            {
+                Title = "Title Only",
+                Description = "Description Only",
+                PosterFileName = "PosterFileName Only",
+                Price = 13.37M
+            };
+            movie2 = con.CreateMovie(movie2);
         }
     }
 }
