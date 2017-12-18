2583e5dfef0388b0858dba12691344872b603c46
triksain <kristian_kamp@hotmail.com>
2017-12-18 22:03:40 +0100

    Eksempel på værdier for brugere på deafult.aspx...

--- BioBookingV2/BioBookingV2/Default.aspx.cs
+++ BioBookingV2/BioBookingV2/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Reflection;
 
 namespace BioBookingV2
 {
@@ -11,7 +12,12 @@ namespace BioBookingV2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string strTemp = "";
+            foreach (PropertyInfo pi in Context.User.Identity.GetType().GetProperties())
+            {
+                strTemp += pi.Name + ": " + pi.PropertyType + ": " + pi.GetValue(Context.User.Identity) + "<br/>";
+            }
+            testLabel.Text = strTemp;
         }
     }
 }
