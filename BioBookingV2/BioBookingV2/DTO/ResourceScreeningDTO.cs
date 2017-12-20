2cfb50b4c5341f089e740e68e763dbdab24acddd
Martin Puge Nielsen <MartinNielsen@Deltek.com>
2017-12-20 12:57:22 +0100

    New DTO

---
+++ BioBookingV2/BioBookingV2/DTO/ResourceScreeningDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BioBookingV2.DTO
+{
+    public class ResourceScreeningDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MovieTitle { get; set; }
+        public string TheaterName { get; set; }
+        public int SeatNumber { get; set; }
+        public int SeatRow { get; set; }
+        public DateTime ScreeningStartDate { get; set; }
+        public DateTime ScreeningEndDate { get; set; }
+        public decimal TicketPrice { get; set; }
+    }
+}
