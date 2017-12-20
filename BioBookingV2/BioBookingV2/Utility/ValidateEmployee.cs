c2c742e0a57c5bfcb6d89772fdca5243ca589e35
Triksain <kristian_kamp@hotmail.com>
2017-12-20 13:37:08 +0100

    Hovsa

---
+++ BioBookingV2/BioBookingV2/Utility/ValidateEmployee.cs
@@ -0,0 +1,31 @@
+﻿using BioBookingV2.DAL;
+using BioBookingV2.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BioBookingV2.Utility
+{
+    public class ValidateEmployee
+    {
+        public bool IsEmployee(string LoginName)
+        {
+            bool Employee = false;
+            List<ResourceDTO> Resource = new List<ResourceDTO>();
+            ResourceDTO resourceSingle = new ResourceDTO();
+            SQLConnector con = new SQLConnector();
+            Resource = con.GetAll("Resource", "LoginName", LoginName, typeof(string)).Cast<ResourceDTO>().ToList();
+            try
+            {
+                resourceSingle = Resource[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            Employee = resourceSingle.Employee;
+            return Employee;
+        }
+    }
+}
