3fd5aa7e3402d529d6769cab7a7ed858da73ee75
Triksain <kristian_kamp@hotmail.com>
2017-12-20 15:58:16 +0100

    View Reservations done

---
+++ BioBookingV2/BioBookingV2/ViewReservations.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewReservations.aspx.cs" Inherits="BioBookingV2.ViewReservations" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div id="BodyDiv" runat="server" style="padding-top: 20px">
+        <asp:Table runat="server" ID="Reservation" class="table table-hover"></asp:Table>
+    </div>
+</asp:Content>
+
---
+++ BioBookingV2/BioBookingV2/ViewReservations.aspx.cs
@@ -0,0 +1,68 @@
+﻿using BioBookingV2.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BioBookingV2.DTO;
+using BioBookingV2.DAL;
+using System.Web.UI.HtmlControls;
+using System.Reflection;
+
+namespace BioBookingV2
+{
+    public partial class ViewReservations : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidateEmployee val = new ValidateEmployee();
+            if (!val.IsEmployee(Context.User.Identity.Name))
+            {
+                Response.Redirect("default.aspx");
+            }
+            SQLConnector con = new SQLConnector();
+            List<ResourceScreeningDTO> lisResScre = new List<ResourceScreeningDTO>();
+            lisResScre = con.GetAll("ResourceScreening").Cast<ResourceScreeningDTO>().ToList();
+            //Lav table
+            HtmlGenericControl table = new HtmlGenericControl("TABLE");
+            table.Attributes["class"] = "table table-hover";
+            BodyDiv.Controls.Add(table);
+
+            //Lav header
+            ResourceScreeningDTO dummy = new ResourceScreeningDTO();
+            TableRow HeaderRow = new TableRow();
+            foreach (PropertyInfo pi in dummy.GetType().GetProperties())
+            {
+                TableCell HeaderCell = new TableCell
+                {
+                    Text = pi.Name
+                };
+                HeaderRow.Cells.Add(HeaderCell);
+            }
+            Reservation.Rows.Add(HeaderRow);
+
+            foreach (ResourceScreeningDTO lisRow in lisResScre)
+            {
+                TableRow CurrentRow = new TableRow();
+                TableCell FirstCell = new TableCell
+                {
+                    Text = lisRow.FirstName
+                };
+                CurrentRow.Cells.Add(FirstCell);
+                foreach (PropertyInfo pi in lisRow.GetType().GetProperties())
+                {
+                    if (pi.Name != "FirstName")
+                    {
+                        TableCell CurrentCell = new TableCell
+                        {
+                            Text = Convert.ToString(pi.GetValue(lisRow))
+                        };
+                        CurrentRow.Cells.Add(CurrentCell);
+                    }
+                }
+                Reservation.Rows.Add(CurrentRow);
+            }
+        }
+    }
+}
